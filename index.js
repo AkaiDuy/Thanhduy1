d2ff8123e374274d97b4d4304f9a36fed53e66ee
Duy <nguyenthanhduy005@gmail.cm>
2017-10-03 10:37:40 +0700

    thay

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.listen(3000);
+
+app.get("/", function(req, res){
+  res.render("trangchu");
+});
